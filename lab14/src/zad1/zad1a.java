c3e62127e425b200a1271017c1c309dd4c17b3bb
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-01-22 13:38:45 +0100

    Zad 1 a)

--- lab14/src/zad1/zad1a.java
+++ lab14/src/zad1/zad1a.java
@@ -1,5 +1,49 @@
 package zad1;
 
+
 public class zad1a {
+    static class TaskThread extends Thread {
+        private boolean isCancelled = false;
+
+        public void cancel() {
+            isCancelled = true;
+            System.out.println("Executed function cancel()");
+        }
+
+        public void run() {
+            // Pętla wykonująca zadanie
+            for (int i = 0; i < 100; i++) {
+                if (isCancelled) {
+                    System.out.println("variable isCancelled was set true");
+                    break;
+                }
+                System.out.println("Execution of " + i + "th iteration");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Utworzenie i uruchomienie wątku wykonującego zadanie
+        final TaskThread taskThread = new TaskThread();
+        taskThread.start();
+
+        // Uruchomienie drugiego wątku, który anuluje zadanie po 5 sekundach
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                taskThread.cancel();
+            }
+        }).start();
+    }
 
 }
